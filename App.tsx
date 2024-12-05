2e02e54171095071766f212a6bcf15d528b5a468
SpaceWAGDev <starlight-caffeine@posteo.org>
2024-12-05 22:32:11 +0100

    Split pages into different functions

--- App.tsx
+++ App.tsx
@@ -5,6 +5,14 @@ import { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import {Button, darkColors, Icon, lightColors} from '@rneui/base';
 
+function cameraPage() {
+
+}
+
+function cardInfo() {}
+
+// TODO: cardLists(cards), cardManager()
+
 export default function App() {
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
@@ -31,7 +39,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
+        <View>
             <Button color='#A95089' onPress={toggleCameraFacing}>
               <Icon name='cameraswitch' color={lightColors.white}/>
             </Button>
@@ -39,6 +47,8 @@ export default function App() {
       </CameraView>
     </View>
   );
+
+  
 }
 
 const styles = StyleSheet.create({
@@ -52,21 +62,5 @@ const styles = StyleSheet.create({
   },
   camera: {
     flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
+  }
 });
