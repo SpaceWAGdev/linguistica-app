94e6c9a605734af02a60b89ba33c7bb8045ec77f
SpaceWAGDev <starlight-caffeine@posteo.org>
2024-12-06 00:25:39 +0100

    Switch to react-native-vision-camera

--- src/pages/cameraPage.tsx
+++ src/pages/cameraPage.tsx
@@ -1,61 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { Camera, runAtTargetFps, useCameraDevice, useFrameProcessor } from 'react-native-vision-camera';
 
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import React, { useState } from 'react';
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import {Button, darkColors, Icon} from '@rneui/base';
-import theme from '../theme';
+export function cameraPage() : React.ReactElement {  
 
-export function cameraPage() {
-    const [facing, setFacing] = useState<CameraType>('back');
-    const [permission, requestPermission] = useCameraPermissions();
+  const device = useCameraDevice("back");
   
-    if (!permission) {
-      // Camera permissions are still loading.
-      return <View/>;
-    }
-  
-    if (!permission.granted) {
-      // Camera permissions are not granted yet.
-      return (
-        <View style={styles.container}>
-          <Text style={styles.message}>We need your permission to show the camera</Text>
-          <Button onPress={requestPermission} title="grant permission" />
-        </View>
-      );
-    }
-  
-    function toggleCameraFacing() {
-      setFacing((current: string) => (current === 'back' ? 'front' : 'back'));
-    }
-  
-    return (
-      <View style={styles.container}>
-        <CameraView style={styles.camera} facing={facing}>
-          <View>
-              <Button color={theme.lightColors?.primary} onPress={toggleCameraFacing}>
-                <Icon name='cameraswitch' color='#fff'/>
-              </Button>
-          </View>
-        </CameraView>
-      </View>
-    );
-  
-    
+
+  if (device == null) return <View/>
+
+  const frameProcessor = useFrameProcessor((frame) => {
+    'worklet'
+
+    runAtTargetFps(10, () => {
+      'worklet'
+      console.log(`${frame.timestamp}: ${frame.width}x${frame.height} ${frame.pixelFormat} Frame (${frame.orientation})`)
+    })
+    }, [])
+
+    return <View>
+      <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+      frameProcessor={frameProcessor}
+    />
+    </View>;
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-    message: {
-      textAlign: 'center',
-      paddingBottom: 10,
-    },
-    camera: {
-      flex: 1,
-    }
-  });
--- src/pages/cardInfo.tsx
+++ src/pages/cardInfo.tsx
@@ -1,14 +1,15 @@
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "@rneui/themed";
 import { View, Text } from "react-native";
+import { Card } from "../card";
 
-export function cardInfo() {
+export function cardInfo(card: Card) {
     const navigation = useNavigation();
     return <View>
-        <Button>
-            <Text>
-                Hello
-            </Text>
-        </Button>
+        <Text>
+            {card.title}
+            {card.id}
+            {card.description}
+        </Text>
     </View>;
 }
